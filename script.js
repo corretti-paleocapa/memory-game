d0d5286c6b40b57f5367ceb64fdc0d529d9f7573
corretti-paleocapa <corretti.alessandro.studente@itispaleocapa.it>
2025-05-29 13:54:56 +0000

    Creato Memory Game

--- script.js
+++ script.js
@@ -156,7 +156,7 @@ document.addEventListener('DOMContentLoaded', () => {
     function startTimer() {
         timerInterval = setInterval(() => {
             seconds = seconds + 10;
-            timerElement.textContent = `Tempo: ${seconds}s`;
+            timerElement.textContent = `Tempo: ${seconds}ms`;
         }, 10);
     }
     
