53fce184816e3783d37ff28e2bac1bd19789a519
corretti-paleocapa <corretti.alessandro.studente@itispaleocapa.it>
2025-05-29 13:07:58 +0000

    Creato Memory Game

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let seconds = 0;
     
     // Emoji da utilizzare come simboli delle carte
-    const symbols = ['🍎', '🍌', '🍒', '🍓', '🍋', '🥝', '🍉', '🥭',':('];
+    const symbols = ['🍎', '🍌', '🍒', '🍓', '🍋', '🥝', '🍉', '🥭',':)','0_0'];
     
     // Inizializza il gioco
     function initGame() {
